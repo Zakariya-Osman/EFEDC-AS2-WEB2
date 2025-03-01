e946ac1beebb0e197b41e8b67c155a59618f8d35
Kenn016 <Kenneth.Cellona@edu.sait.ca>
2025-03-01 14:19:33 -0700

    Add comments to Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,28 @@
 import React from 'react'
 
-function Navbar() {
+/**
+ * Navbar Component
+ * 
+ * Name: Kenneth Cellona
+ * Date: March 1, 2025
+ * Description:
+ * A simple navigation bar with links to Home, Add Student, About, and Student List.
+ * It's styled with Tailwind CSS and stays fixed at the top of the page.
+ * 
+ * No user input, just a static menu. Outputs a responsive navbar.
+ */
+
+const Navbar = () => {
   return (
-    <div>Navbar</div>
+    // Navigation bar with fixed position and padding
+    <nav className="bg-gray-800 p-4 fixed w-full">
+      <ul className="flex space-x-6 justify-center"> 
+        <li className="text-white hover:underline cursor-pointer px-4">Home</li>
+        <li className="text-white hover:underline cursor-pointer px-4">Add Student</li>
+        <li className="text-white hover:underline cursor-pointer px-4">About</li>
+        <li className="text-white hover:underline cursor-pointer px-4">Student List</li>
+      </ul>
+    </nav>
   )
 }
 
