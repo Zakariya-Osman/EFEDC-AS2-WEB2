0f0d084584f59f87d3739d49e96535bc519b1e5e
Zackaria Osman <Zackaria.Osman@edu.sait.ca>
2025-02-28 18:59:34 -0700

    Added to components
    - Added to Nav bar
    - Added to Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,7 @@
-import React from 'react'
-
-function Footer() {
+export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="bg-purple-700 text-white text-center py-4 mt-10">
+      <p>&copy; 2025 Website. All Rights Reserved.</p>
+    </footer>
+  );
 }
-
-export default Footer
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,20 @@
-import React from 'react'
-
-function Navbar() {
+export default function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <nav className="bg-purple-700 text-white py-4 w-full shadow-lg">
+      <div className="container mx-auto flex justify-between items-center px-6">
+        {/* School Name */}
+        <h1 className="text-2xl font-bold">Somerset Alabama Igloo of Technology</h1>
+        
+        {/* Navigation Links */}
+        <ul className="flex space-x-6">
+          <li><a href="#" className="hover:text-gray-300">Home</a></li>
+          <li><a href="#" className="hover:text-gray-300">About Us</a></li>
+          <li><a href="#" className="hover:text-gray-300">Academics</a></li>
+          <li><a href="#" className="hover:text-gray-300">Admissions</a></li>
+          <li><a href="#" className="hover:text-gray-300">Student Life</a></li>
+          <li><a href="#" className="hover:text-gray-300">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
-
-export default Navbar
