937456a8619adfdb05db5b792351c8f278e5c617
JakobHoward1 <166176698+JakobHoward1@users.noreply.github.com>
2025-03-01 15:21:44 -0700

    Update Footer.js
    updated comments

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,13 @@
 // jakob howard - footer
 import React from 'react'; //client side react component
 
-/** Footer page
+/** 
+ * StudentForm Component
+ * Author: Jakob Howard
+ * Date: 3/1/2025
+
+
+Footer page
 contains information regarding the school such as name and contact information, as well as resources for parents to use.
 Information is seperated into 3 different column with different titles for organization and readability, text is white on a black background to ensure the
 footer is accessable and readable for everyone.
@@ -11,19 +17,19 @@ function Footer() {
   return (
     <footer className="bg-black text-white text-center p-6 mt-6">
       <div className="grid grid-cols-3 gap-8">
-        <div> // div for first column, contains school infomation
+        <div> {/* div for first column, contains school infomation*/}
           <h3 className="font-semibold text-lg">New Generation High School</h3>
           <p>(403) 284-7248</p>
           <p>1301 16 Ave NW, Calgary, AB T2M 0L4</p>
         </div>
 
-        <div> // div for second column, contains volunter and school concil info
+        <div> {/* div for second column, contains volunter and school concil info*/}
           <h3 className="font-semibold text-lg">Parents & Students</h3>
           <a>School Council</a>
           <a>Volunteer</a>
         </div>
 
-        <div> // div for third column, contains resouces for parents
+        <div> {/* div for third column, contains resouces for parents*/}
           <h3 className="font-semibold text-lg">Support For Parents</h3>
           <a href="https://d2l.cbe.ab.ca/d2l/loginh/" className="text-white underline">Brightspace</a>
           <a>Google Workspace</a>
